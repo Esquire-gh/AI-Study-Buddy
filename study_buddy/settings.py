fab53da5b71ad296439cd7f4161542df87879ce9
Richard Ackon <ackon.k.richard@gmail.com>
2024-04-07 08:40:04 -0400

    update env reading

--- study_buddy/settings.py
+++ study_buddy/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import environ
 
@@ -20,6 +21,9 @@ env = environ.Env(
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
